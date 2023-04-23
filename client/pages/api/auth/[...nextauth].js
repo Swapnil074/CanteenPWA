9e2e5037ac07b8ad1e9ce75b7489fd9341ecaba0
Swapnil074 <mailswapnil0074@gmail.com>
2023-04-23 18:39:19 +0530

    cleanup

--- client/pages/api/auth/[...nextauth].js
+++ client/pages/api/auth/[...nextauth].js
@@ -2,8 +2,6 @@ import NextAuth from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
 import { MongoDBAdapter } from '@next-auth/mongodb-adapter'
 import clientPromise from '@/lib/mongodb'
-import { FirestoreAdapter } from '@next-auth/firebase-adapter'
-import { db } from '@/firebase.config'
 
 export default NextAuth({
   providers: [
